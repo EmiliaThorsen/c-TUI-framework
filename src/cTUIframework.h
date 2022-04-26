bc781f8df309a0de7e592e7b83a2460529de6f7c
Emilia Thorsen <samuel.j.thors@gmail.com>
2022-04-26 15:50:52 +0200

    write small explainer in readme and add it to main h file

--- src/cTUIframework.h
+++ src/cTUIframework.h
@@ -1,12 +1,38 @@
+int initTUI (); //initiate cTUI
+int destroyTUI(); //destroy cTUI
+int updateTUI(); //update keystroke handler
+int renderTUI(struct TUI tui); //render the TUI struct
 
 
-int initTUI ();
-int destroyTUI();
-int updateTUI();
-int renderTUI();
+union container {
+    struct split *split;
+    struct content *content;
+};
 
 
-// update function for keystroke handleing
-// render function to render ui
-// way of adding and removing layout components
-// split sections + hovering windows
+struct content {
+    char **data; //array of strings to write to screen
+};
+
+
+struct split {
+    int *type; //array of the type of the splits
+    int splits; //amout of splits
+    union container *split; //contents of the splits
+};
+
+
+struct tab {
+    char *name; //name of the tab
+    int type; //the type of content
+    union container *content; //content of the tab, either content or a split
+};
+
+
+//main struct for the TUI
+struct TUI {
+    int tabs; //the amount of tabs
+    struct tab *tab; //array of tabs
+    char *barRight; //text on the right side of the status bar
+    char *barLeft; //text on the left side of the status bar
+};
